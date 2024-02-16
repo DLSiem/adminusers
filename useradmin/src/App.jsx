a89d975036d44bfad8fb7e80e59bf35e4f3ccc24
DLSiem <pdlsiem17@gmail.com>
2024-02-16 19:13:38 +0530

    Making Dashboard Private

--- useradmin/src/App.jsx
+++ useradmin/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Dashboard, Home, NotFound, Signin, Signup, Users } from "./pages";
 import { Header, Foot } from "./components";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -10,6 +11,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/Users" element={<Users />} />
